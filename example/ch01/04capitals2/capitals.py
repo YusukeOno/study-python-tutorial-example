ac32eb14d2c35fa436d294d1857f8a7fd52a3223
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-04-10 22:28:54 +0900

    Chapter01

---
+++ example/ch01/04capitals2/capitals.py
@@ -0,0 +1,9 @@
+def get_united_states_capitals():
+    us_capitals_by_state = {
+        'Alabama': 'Montgomery',
+        'Alaska': 'Juneau',
+        'Wyoming': 'Cheyenne',
+    }
+
+    capitals = us_capitals_by_state.values()
+    return sorted(capitals)
---
+++ example/ch01/05capitals3/capitals.py
@@ -0,0 +1,7 @@
+US_CAPITALS_BY_STATE = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Wyoming': 'Cheyenne',
+}
+
+US_CAPITALS = sorted(US_CAPITALS_BY_STATE.values())
