578e344e52ef72838b6345c8e8e9543a82b64573
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-04-10 23:04:57 +0900

    Chapter02-04

---
+++ example/ch02/04modules/receipt1.py
@@ -0,0 +1,1 @@
+# from sales_tax import add_sales_tax, add_state_tax, add_city_tax, add_local_millage_tax
---
+++ example/ch02/04modules/receipt2.py
@@ -0,0 +1,6 @@
+# from sales_tax import {
+#     add_sales_tax,
+#     add_state_tax,
+#     add_city_tax,
+#     add_local_millage_tax,  # 財産にかかる税
+# }
