f84dc223b9e3ce7f3f43b1b9f1809ba5c57a1e2d
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-04-15 15:59:01 +0900

    Chapter04-04

---
+++ example/ch04/04counting/counting1.py
@@ -0,0 +1,18 @@
+# 第1版
+def get_number_with_highest_count(counts):
+    max_count = 0
+    for number, count in counts.items():
+        if count > max_count:
+            max_count = count
+            number_with_highest_count = number
+    return number_with_highest_count
+
+
+def most_frequent(numbers):
+    counts = {}
+    for number in numbers:
+        if number in counts:
+            counts[number] += 1
+        else:
+            counts[number] = 1
+    return get_number_with_highest_count(counts)
