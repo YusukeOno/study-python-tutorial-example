2f128d0e56e8a66f7a3608b374f485254caa8001
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-04-16 08:04:06 +0900

    Chapter04-05

---
+++ example/ch04/05timing/timing2.py
@@ -0,0 +1,19 @@
+import random
+
+
+def sort_expensive():
+    the_list = random.sample(range(1_000_000), 1000)  # _は桁区切り
+    the_list.sort()
+
+
+def sort_cheap():
+    the_list = random.sample(range(1_000), 10)
+    the_list.sort()
+
+
+if __name__ == '__main__':
+    sort_expensive()
+    for i in range(1000):
+        sort_cheap()
+
+# python -m cProfile --sort cumtime ./example/ch04/05timing/timing2.py
