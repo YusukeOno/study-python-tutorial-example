bb8744e333e720d83d7a4d90a8a017e57accc12d
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-04-15 15:46:14 +0900

    Chapter04-03

---
+++ example/ch04/03generator/generators.py
@@ -0,0 +1,19 @@
+# rangeの処理を真似たもの
+def range(*args):
+    if len(args) == 1:
+        start = 0
+        stop = args[0]
+    else:
+        start = args[0]
+        stop = args[1]
+
+    current = start
+
+    while current < stop:
+        yield current
+        current += 1
+
+
+def squares(items):
+    for item in items:
+        yield item ** 2
