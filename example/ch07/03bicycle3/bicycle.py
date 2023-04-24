2ec469c38754a91af61a91f43f92fb6a9a7410fc
小野裕輔 <ono.yusuke@nsw-ss.co.jp>
2023-04-24 22:50:36 +0900

    Chapter07-03

---
+++ example/ch07/03bicycle3/bicycle.py
@@ -0,0 +1,29 @@
+class CarbonFiberFrame:
+    def __repr__(self):
+        return 'カーボンファイバーのフレーム'
+
+
+class Tire:
+    def __repr__(self):
+        return 'ゴムのタイヤ'
+
+
+class Frame:
+    def __repr__(self):
+        return 'アルミのフレーム'
+
+
+class Bicycle:
+    def __init__(self, front_tire, back_tire, frame):
+        self.front_tire = front_tire
+        self.back_tire = back_tire
+        self.frame = frame
+
+    def print_specs(self):
+        print(f'フレーム: {self.frame}')
+        print(f'前のタイヤ: {self.front_tire}。後ろのタイヤ: {self.back_tire}')
+
+
+if __name__ == '__main__':
+    bike = Bicycle(Tire(), Tire(), CarbonFiberFrame())
+    bike.print_specs()
